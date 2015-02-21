9bffa1f2db8c648a717e145405a293562d3415eb
Yujian Zhang <yujian.zhang@gmail.com>
2015-02-21 20:47:21 +0800

    Factor out FilterArrayAdapter to package scaland.

--- src/main/scala/CalendarArrayAdapter.scala
+++ src/main/scala/CalendarArrayAdapter.scala
@@ -13,13 +13,13 @@ package net.whily.android.calendarlookup
 
 import java.text.Normalizer
 import android.content.Context
-import android.widget.{ArrayAdapter, Filterable, Filter}
+import net.whily.scaland.FilterArrayAdapter
 
 /**
  * Filter the string specific for calendar input.
  */
 class CalendarArrayAdapter(context: Context, textViewResourceId: Int, objects: Array[String])
-  extends ArrayAdapter[String](context, textViewResourceId, objects) with Filterable {
+  extends FilterArrayAdapter(context, textViewResourceId, objects) {
 
   /** Normalize string. 
     * 
@@ -27,51 +27,8 @@ class CalendarArrayAdapter(context: Context, textViewResourceId: Int, objects: A
     * 2. For string starting from Chinese, convert numeric into Chinese.
     * 3. Convert all simplified Chinese into Traditional Chinese.
     */
-  private def normalizeString(s: String): String = {
+  override def normalize(s: String): String = {
     val t = Normalizer.normalize(s.toLowerCase, Normalizer.Form.NFD)
     t
-  }
-
-  /* Keep the original data and corresponding accent-folded version. */
-  private val foldedPair =
-    for (s <- objects) yield (s, normalizeString(s))
-
-  /* The actual data displayed. */
-  private var data: Array[String] = objects
-
-  private var filter: Filter = null
-
-  override def getCount(): Int = data.length
-  override def getItem(index: Int): String = data(index)
-
-  override def getFilter(): Filter = {
-    if (filter == null) filter = new CustomFilter()
-    filter
-  }
-
-  private class CustomFilter extends Filter {
-    override protected def performFiltering(constraint: CharSequence): Filter.FilterResults = {
-      var filterResults = new Filter.FilterResults
-      if (constraint == null || constraint.length == 0) {
-        filterResults.values = objects
-        filterResults.count = objects.length
-      } else {
-        val query = normalizeString(constraint.toString)
-        val filteredData = for (item <- foldedPair if item._2.contains(query))
-          yield item._1
-        filterResults.values = filteredData
-        filterResults.count = filteredData.length
-      }
-      filterResults
-    }
-
-    override protected def publishResults(constraint: CharSequence, results: Filter.FilterResults) {
-      data = results.values.asInstanceOf[Array[String]]
-      if (results != null && results.count > 0) {
-        notifyDataSetChanged()
-      } else {
-        notifyDataSetInvalidated()
-      }
-    }
   }
 }
