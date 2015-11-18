ee7550308bcf094d8f8e91de78c60fb6dacfb5f3
Yujian Zhang <yujian.zhang@gmail.com>
2015-11-18 21:49:12 +0800

    For searchEntry text, normalize to traditional Chinese.

--- src/main/scala/CalendarArrayAdapter.scala
+++ src/main/scala/CalendarArrayAdapter.scala
@@ -14,6 +14,7 @@ package net.whily.android.calendarlookup
 import java.text.Normalizer
 import android.content.Context
 import net.whily.scaland.FilterArrayAdapter
+import net.whily.chinesecalendar.ChineseCalendar
 
 /**
  * Filter the string specific for calendar input.
@@ -21,14 +22,14 @@ import net.whily.scaland.FilterArrayAdapter
 class CalendarArrayAdapter(context: Context, textViewResourceId: Int, objects: Array[String])
   extends FilterArrayAdapter(context, textViewResourceId, objects) {
 
-  /** Normalize string. 
-    * 
+  /** Normalize string.
+    *
     * 1. For string starting from numeric (1, 2, 3), convert all Chinese number into numeric.
     * 2. For string starting from Chinese, convert numeric into Chinese.
     * 3. Convert all simplified Chinese into Traditional Chinese.
     */
   override def normalize(s: String): String = {
     val t = Normalizer.normalize(s.toLowerCase, Normalizer.Form.NFD)
-    t
+    ChineseCalendar.simplified2Traditional2(t)
   }
 }
